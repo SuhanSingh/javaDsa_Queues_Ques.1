13c64c4c95981e4ac320e5cd2adde0e802133667
Suhani Singh <129667648+SuhanSingh@users.noreply.github.com>
2023-12-30 19:41:25 +0530

    Add files via upload

---
+++ Queues.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.LinkedList;
+public class Queues {
+	static void generatePrintBinary(int n) { //TC = O(n) SC = O(n)
+		Queue<String> q = new LinkedList<String>();
+		q.add("1");
+	
+		while(n-- > 0) {
+			String s1 = q.peek();
+			q.remove();
+			System.out.println(s1);
+			String s2 = s1;
+			q.add(s1 + "0");
+			q.add(s2 + "1");
+		}
+	}
+
+	public static void main(String args[]) {
+		int n = 10;
+		generatePrintBinary(n);
+	}
+}
+// 1
+// 10
+// 11
+// 100
+// 101
+// 110
+// 111
+// 1000
+// 1001
+// 1010
